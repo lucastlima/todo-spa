8ae9765fd26fa7ab581a129dbddff33bcc23a7f1
Lucas Lima <lucastlima86@gmail.com>
2020-02-21 00:48:28 +0000

    Setup Project

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  display: flex;
+  flex: 1;
+  color: white;
+`;
 
 function App() {
-  return <div>Hello React!</div>;
+  return <StyledApp>Hello React!</StyledApp>;
 }
 
 export default App;
